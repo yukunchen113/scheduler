a38c921b56ff4c110107b7dae4aaaa899e998e8f
yukunchen113 <yukunchen113@gmail.com>
2023-11-02 23:41:57 -0400

    uncommented

--- plex/daily/base.py
+++ plex/daily/base.py
@@ -10,12 +10,12 @@ from plex.daily.template import update_templates_in_file
 
 def process_daily_file(filename: str) -> None:
     update_templates_in_file(filename)
-    # timings = get_timing_from_file(filename)
-    # read_tasks = read_taskgroups(filename)
-    # if not read_tasks:
-    #     taskgroup = get_taskgroup_from_timing_configs(timings)
-    #     taskgroup = calculate_times_in_taskgroup(taskgroup)
-    #     tasks_to_write = [taskgroup]
-    # else:
-    #     tasks_to_write = sync_taskgroups_with_timing(timings, read_tasks)
-    # write_taskgroups(tasks_to_write, filename)
+    timings = get_timing_from_file(filename)
+    read_tasks = read_taskgroups(filename)
+    if not read_tasks:
+        taskgroup = get_taskgroup_from_timing_configs(timings)
+        taskgroup = calculate_times_in_taskgroup(taskgroup)
+        tasks_to_write = [taskgroup]
+    else:
+        tasks_to_write = sync_taskgroups_with_timing(timings, read_tasks)
+    write_taskgroups(tasks_to_write, filename)
