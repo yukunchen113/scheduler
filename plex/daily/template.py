6568489aed3f66a16a1c0713d5881b505e03adaf
yukunchen113 <yukunchen113@gmail.com>
2023-11-02 23:40:26 -0400

    added template processing

--- plex/daily/template.py
+++ plex/daily/template.py
@@ -7,13 +7,10 @@ where ... is a valid template specifier.
 
 ... can be:
 y
-x:y
-x:y:z
 y:z
 
-in the template folder (default: {project base dir}/routine/):
+in the template folder (default: {project base dir}/routines/):
 - y is a file (if no x, then is in the root template folder)
-- x is a folder that y is in
 - z is a category.
 
 A file can be structured as:
@@ -31,39 +28,77 @@ timing spec
 '''
 in which case {y:x} can be used
 """
+from typing import Union
 import re
 from plex.daily.config_format import SPLITTER
+import os
+import glob
 
-TEMPLATE_PATTERN = r"\{(?:(.*)\:)?(.*)(?:\:(.*))?\}\n"
+TEMPLATE_PATTERN = r"\{([^:]*)(?:\:([^:]*))?\}\n"
+TEMPLATE_BASE_DIR = "routines"
 
 
-def read_template_from_timing(filename: str) -> list[str]:
+def read_sections_from_template(filename: str) -> dict[str, list[str]]:
+    sections: dict[str, list[str]] = {}
+    with open(filename) as f:
+        lines = f.readlines()
+        last_key = None
+        for line in lines:
+            if line.endswith(":\n"):
+                last_key = line.replace(":\n", "")
+                sections[last_key] = []
+            elif last_key is not None:
+                sections[last_key].append(line)
+    return sections
+
+
+def read_template_from_timing(filename: str) -> dict[str, list[str]]:
     """
     reads the {} templates in timing file
     """
     with open(filename) as f:
         lines = f.readlines()
-    templates: list[str] = []
+    templates: dict[str, list[str]] = {}
     for line in lines:
         if re.match(TEMPLATE_PATTERN, line):
-            print(re.findall(TEMPLATE_PATTERN, line))
-            folder, dfile, section = re.findall(TEMPLATE_PATTERN, line)[0]
-            print("folder: \t", folder)
-            print("dfile: \t", dfile)
-            print("section: \t", section)
-            # templates.append()
+            dfile, section = re.findall(TEMPLATE_PATTERN, line)[0]
+            path = os.path.join(TEMPLATE_BASE_DIR, dfile+".*")
+            files = glob.glob(path)
+            if len(files) != 1:
+                print(
+                    f"pattern '{line}' returned '{files}', must be "
+                    "exactly 1 file. Ignoring pattern.")
+                continue
+            file = files[0]
+            tsections = read_sections_from_template(file)
+            if section:
+                timing_lines = tsections[section]
+            else:
+                timing_lines = sum(tsections.values(), [])
+            templates[line] = timing_lines
         if line.startswith(SPLITTER):
             return templates
+    return templates
 
 
-def write_timings_inplace_of_template(filename: str) -> None:
+def write_timings_inplace_of_template(filename: str, replacements: dict[str, list[str]]) -> None:
     """replaces the templates with the routines in the timing file
     """
-
-
-def get_specs_for_template(template: str) -> dict[str, list[str]]:
-    """gets a set of specifictions given a template"""
+    with open(filename) as f:
+        lines = f.readlines()
+    new_lines = []
+    for idx,line in enumerate(lines):
+        if line.startswith(SPLITTER):
+            new_lines += lines[idx:]
+            break
+        if line in replacements:
+            new_lines+=replacements[line]
+        else:
+            new_lines.append(line)
+    with open(filename,"w") as f:
+        f.write("".join(new_lines))
 
 
 def update_templates_in_file(filename: str) -> None:
-    read_template_from_timing(filename)
+    replacements = read_template_from_timing(filename)
+    write_timings_inplace_of_template(filename, replacements)
