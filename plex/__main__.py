9e0f457156d9b4cfafa3517c75420b5eb917ea29
yukunchen113 <yukunchen113@gmail.com>
2023-11-26 16:45:42 -0500

    fixed cal bug, updated cmdline interface to be better UX

--- plex/__main__.py
+++ plex/__main__.py
@@ -12,35 +12,52 @@ DAILY_BASEDIR = "daily"
 
 
 def main(
-    date: str,
+    tomorrow: bool = False,
+    date: Optional[str] = None,
     filename: Optional[str] = None,
-    process_daily: bool = False,
-    push_to_calendar: bool = False,
-    sync_calendar: bool = False,
+    no_process_daily: bool = False,
+    push: bool = False,
+    sync: bool = False,
 ) -> None:
-    try:
-        date = datetime.datetime.strptime(date, "%Y-%m-%d")
-        datestr = date.strftime("%Y-%m-%d")
-    except ValueError:
-        raise ValueError(
-            f"Invalid datestr '{datestr}' specified. must be in form 'YYYY-MM-DD'"
-        )
+    """Plex: Planning and execution command line tool
+
+    Args:
+        tomorrow (bool, optional): if process tomorrow's file. Defaults to False.
+        date (Optional[str], optional): process specified date. Defaults to None.
+        filename (Optional[str], optional): process specifed filename, will still need to provide specified date for calendar sync. Defaults to None.
+        no_process_daily (bool, optional): don't process the daily file. Defaults to False.
+        push (bool, optional): one time push to google calendar. Defaults to False.
+        sync (bool, optional): start up sync server to sync calendar and file, while processing tasks. Defaults to False.
+    """
+    if date:
+        assert not tomorrow, 'cannot specify tomorrow when date is specified.'
+        try:
+            # validate datestr
+            datestr = datetime.datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d")
+        except ValueError:
+            raise ValueError(
+                f"Invalid datestr '{datestr}' specified. must be in form 'YYYY-MM-DD'"
+            )
+    elif tomorrow:
+        datestr = (datetime.datetime.today()+datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    else:
+        datestr = datetime.datetime.today().strftime("%Y-%m-%d")
 
     if filename is None:
         filename = datestr
-    filename = make_daily_filename(filename, process_daily)
-    if process_daily:
+    filename = make_daily_filename(filename, not no_process_daily)
+    if not no_process_daily:
         process_daily_file(datestr, filename)
 
-    if sync_calendar:
-        assert not push_to_calendar
+    if sync:
+        assert not push
         if not os.path.exists(filename):
             raise ValueError(
                 f"Daily file {filename} doesn't exist. Unable to sync to calendar."
             )
         sync_tasks_to_calendar(datestr, filename, push_only=False)
 
-    elif push_to_calendar:
+    elif push:
         if not os.path.exists(filename):
             raise ValueError(
                 f"Daily file {filename} doesn't exist. Unable to push to calendar."
